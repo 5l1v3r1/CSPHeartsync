e9e47992d7cc5c0af5dc52d1510e5838f9a34363
vietan <vietanisme@gmail.com>
2018-01-17 13:36:26 +0700

    validify_url

--- modules/validify_url/index.js
+++ modules/validify_url/index.js
@@ -1,27 +1,24 @@
-var check_url_validity = (str) => {
-    var pattern = new RegExp('^(https?:\/\/)?' + // protocol
-        '((([a-z\d]([a-z\d-]*[a-z\d])*)\.)+[a-z]{2,}|' + // domain name
-        '((\d{1,3}\.){3}\d{1,3}))' + // OR ip (v4) address
-        '(\:\d+)?(\/[-a-z\d%_.~+]*)*' + // port and path
-        '(\?[;&a-z\d%_.~+=-]*)?' + // query string
-        '(\#[-a-z\d_]*)?$', 'i'); // fragment locater
-    if (!pattern.test(str)) {
-        return false;
-    } else {
+var valid_url = require ('valid-url');
+var check_url_validity = (url) => {
+    if (valid_url.isUri (url))
+    {
         return true;
+    }
+    else 
+    {
+        return false;
     }
 }
 
 var validify = (str) => {
     var spl = str.split ('.');
-    var res;
+    var res = "";
     for (var i = 1; i < spl.length; ++i)
     {
-        res += spl[i];
+        res += spl[i] + ".";
     }
-    return spl;
+    return res;
 }
-
 module.exports = {
     check_url_validity: check_url_validity,
     validify: validify
