5e4b9134545e728fc5f0d58dae3ac6c7670defd6
vietan <vietanisme@gmail.com>
2018-01-19 09:17:21 +0700

    fix bug xong roi hahah

--- modules/bot.js
+++ modules/bot.js
@@ -15,8 +15,9 @@ class asyncBot {
         if (textInput.toLowerCase() === 'help') {
             checkincovers.checkincovers(senderId).then(inconvers => {
                 if (inconvers === 2) {
-                    let partnerId = await (getPartner.getPartner(senderId));
-                    sendMessage.sendTextMessage(partnerId, textInput);
+                    getPartner.getPartner(senderId).then(partnerId => {
+                        sendMessage.sendTextMessage(partnerId, textInput);
+                    })
                 } else {
                     sendTextMessage(senderId, "Gõ một từ bất kỳ để bắt đầu một cuộc trò chuyện. Bạn cũng có thể bấm vào mục bắt đầu trò chuyện ở menu chatbot")
                     sendTextMessage(senderId, "Gõ \"end\" khi đang trò chuyện để kết thúc cuộc trò chuyện đó, hoặc khi đang ở trong hàng đợi để thoát khỏi hàng đợi")
