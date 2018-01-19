95043aa1bc887a208c76bcb59a9ad8a636e087fb
vietan <vietanisme@gmail.com>
2018-01-19 09:31:57 +0700

    Revert "post-final bug fix"
    This reverts commit 74c962e07ae8b6130a43c318433b7e523165b433.

--- modules/bot.js
+++ modules/bot.js
@@ -104,26 +104,6 @@ class asyncBot {
                     this.reply(senderId, "start");
                     break;
                 }
-            case "SELECT_MALE":
-                {
-                    this.select(senderId, 'male');
-                    break;
-                }
-            case "SELECT_FEMALE":
-                {
-                    this.select(senderId, 'female');
-                    break;
-                }
-            case "SELECT_ANY":
-                {
-                    this.select(senderId, 'none');
-                    break;
-                }
-            case "CHANGE_FAV":
-                {
-                    sendMessage.sendButtonSelectGender(senderId)
-                    break;
-                }
         }
     }
     select(senderId, gender) {
