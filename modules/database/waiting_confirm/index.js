2528d1f7c0efbde7e0fc292ed8269a23ca1b8562
Viet An <vietanisme@gmail.com>
2018-01-21 10:35:54 +0700

    test confirmation

---
+++ modules/database/waiting_confirm/index.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+var mongodb = require('mongodb').MongoClient,
+    url = process.env.URL_DB;
+
+var add_awaiting_input = (senderId, fburl, text) => {
+    return new Promise((resolve, reject) => {
+        mongodb.connect(url, (err, db) => {
+            if (err) throw err;
+            db.db('cspheartsync').collection('input_pending').insertOne({
+                id: senderId,
+                type: 'confirm',
+                fburl: fburl,
+                mess: text
+            })
+        })
+    })
+}
+
+var remove_awaiting_input = (senderId) => {
+    return new Promise((resolve, reject) => {
+        mongodb.connect(url, (err, db) => {
+            if (err) throw err;
+            db.db('cspheartsync').collection('input_pending').deleteMany(
+                {
+                    id: senderId,
+                }
+            )
+        })
+    })
+}
+
+module.exports = {
+    add : add_awaiting_input,
+    remove : remove_awaiting_input
+}
