94764dc789a0a91704c23348c72d646a7e423270
Viet An <vietanisme@gmail.com>
2018-01-21 16:08:55 +0700

    aaa

--- modules/database/send_anonymous_message/index.js
+++ modules/database/send_anonymous_message/index.js
@@ -33,7 +33,6 @@ var send_message = (message, fburl, message_type) => {
             if (img_id == 'not found') {
                 resolve('not found')
             } else {
-                console.log (message);
                 mongodb.connect(url, (err, dbase) => {
                     if (err) throw err;
                     img_id = parseInt(img_id).toString()
